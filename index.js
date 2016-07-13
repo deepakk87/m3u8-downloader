f64437aed5506ea5bbe51504ce3d36a4f8d9b030
Deepak Kumar <deepak.kumar@xseededucation.com>
2016-07-13 14:36:25 +0530

    Working

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-var dlink = 'http://localhost:3010/getVideos/sample/index.m3u8';
+var Downloader = require('./lib/downloader.js');
+var options = {
+  host: 'localhost',
+  port : 3010,
+  path: '/getVideos/sample/index.m3u8'
+};
+
+var downloader = new Downloader(options, 'tmpVideos/sample');
+downloader.startDownload();
+downloader.on('finished', function (){
+    console.log('Finished');
+});
+
+downloader.on('eror', function(){
+    console.log('Error');
+});
+
+downloader.on('progress', function(){
+    console.log('progress');
+});
